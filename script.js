d51f19fff86366702d73eb8cc8e540b235fd8380
charles <ibehcharles347@gmail.com>
2025-10-25 15:46:49 +0100

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', ()=> {
+document.addEventListener('DOMContentLoaded', () => {
     let myForm = document.getElementById('login_form');
     let myModal = new bootstrap.Modal(document.getElementById('staticBackdrop'));
     let hiddenInput1 = document.querySelector('.input1')
@@ -9,46 +9,48 @@ document.addEventListener('DOMContentLoaded', ()=> {
     let passwordErrorMsg = document.querySelector('.passworderrormessage');
 
 
-     hiddenInput1.style.display = 'none';
-     hiddenInput2.style.display = 'none';
+    hiddenInput1.style.display = 'none';
+    hiddenInput2.style.display = 'none';
 
-     myForm.addEventListener('submit', (e)=> {
+    myForm.addEventListener('submit', (e) => {
         e.preventDefault();
-        
+        let email = document.getElementById('inputcont1');
+        let password = document.getElementById('inputcont2');
         let inputEmail1 = document.getElementById('email_input1').value;
-        let inputEmail2 = document.getElementById('email_input2').value;
+        // let inputEmail2 = document.getElementById('email_input2').value;
         let inputPassword1 = document.getElementById('password_input1').value;
-        let inputPassword2 = document.getElementById('password_input2').value; 
+        // let inputPassword2 = document.getElementById('password_input2').value;
         let checkboxInput
 
-
-        const userEmail = ['ibehcharles347@gmail.com', 'ibehmarcel@yahoo.com', 'ebuka@email.com'];
-        const userPassword = ['ososos', '12345', '78910']
-
-        if (userEmail.includes(inputEmail1) || userEmail.includes(inputEmail2)){
-            console.log(inputEmail1);
-            console.log(inputEmail2);
-        }else{
+        let users = [
+            { username: 'Charles', userEmail: 'ibehces347@gmail.com', userPassword: 'Abuja' },
+            { username: 'Wayne', userEmail: 'ibehcharles347@gmail.com', userPassword: 'Lagos' },
+            { username: 'Carti', userEmail: 'iehcharles347@gmail.com', userPassword: 'Ondo' },
+            { username: 'Keno', userEmail: 'ibehcharle7@gmail.com', userPassword: 'Enugu' },
+            { username: 'Kilolo', userEmail: 'ibhcharles347@gmail.com', userPassword: 'Imo' },
+            { username: 'Onome', userEmail: 'ibehchles347@gmail.com', userPassword: 'Kogi' }
+        ]
+        let isMatch = users.some((user) => user.userEmail === inputEmail1 && user.userPassword === inputPassword1);
+        if (isMatch) {
+            myModal.show();
+        } else {
             let emailError = document.createElement('p')
             emailError.textContent = 'Your email is incorrect or empty. Please check and try again.';
             emailErrorMsg.innerHTML = "";
             emailErrorMsg.append(emailError);
             hiddenInput1.style.display = 'flex';
-            hideInput1.style.display = 'none';
-        }
-        if (userPassword.includes(inputPassword1) || userPassword.includes(inputPassword2)){
-            myModal.show();
-
-        }else{
             let passwordError = document.createElement('p')
             passwordError.textContent = 'Your password is incorrect or empty. Please check and try again.';
             passwordErrorMsg.innerHTML = "";
             passwordErrorMsg.append(passwordError);
             hiddenInput2.style.display = 'flex';
-            hideInput2.style.display = 'none';
+            email.classList.add('error');
+            password.classList.add('error');
+            email.classList.add('erroricon');
+            password.classList.add('erroricon');
         }
 
-     })
+    })
 
 
 })
